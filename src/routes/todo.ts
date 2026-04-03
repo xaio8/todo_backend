fd4927f464b77469e846c6243ec5ea8fd7220a63
xaio8 <kyaltalkon80@gmail.com>
2026-04-03 16:34:47 +0630

    add local postgre db

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,7 +1,11 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import * as todoController from "../controller/todo_controller.js";
 
 const todoRoute = Router();
 
-todoRoute.get("/", (req: Request, res: Response) => res.json({ todo: [] }));
+todoRoute.get("/", todoController.getAllTodos);
+todoRoute.post("/", todoController.createTodo);
+// todoRoute.patch("/:id/status", todoController.updateTodoStatus);
+todoRoute.delete("/:id", todoController.deleteTodo);
 
 export default todoRoute;
